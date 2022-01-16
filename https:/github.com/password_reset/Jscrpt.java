029351808c432f24656fd048f73cc8924b1d43de
Gerardojc <79390510+Gerardojc@users.noreply.github.com>
2022-01-16 01:07:40 -0600

    Add files via upload

---
+++ https:/github.com/password_reset/Jscrpt.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+ // Compiler version JDK 11.0.2
+
+ class Dcoder
+ {
+   public static void main(String args[])
+   { 
+    System.out.println("Hello, Dcoder!");
+   }
+ }
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<ref/>Reference 1</ref> Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.<ref>Reference 2</ref> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
